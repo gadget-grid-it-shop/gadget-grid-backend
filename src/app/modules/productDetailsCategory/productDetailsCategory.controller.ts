7ea72d495653b52425004ec0069da28c07d20958
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2024-08-02 01:22:40 +0600

    update product details category

--- src/app/modules/productDetailsCategory/productDetailsCategory.controller.ts
+++ src/app/modules/productDetailsCategory/productDetailsCategory.controller.ts
@@ -2,6 +2,7 @@ import {ProductDetailsCategoryServices} from "./productDetailsCategory.service";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
 import httpStatus from "http-status";
+import {Types} from "mongoose";
 
 const createProductDetailsCategory = catchAsync(async (req, res) => {
   const payload = req.body;
@@ -16,6 +17,20 @@ const createProductDetailsCategory = catchAsync(async (req, res) => {
   });
 });
 
+const updateProductDetailsCategory = catchAsync(async (req, res) => {
+  const id = req.params.id;
+  const payload = req.body;
+  const result = await ProductDetailsCategoryServices.updateProductDetailsCategoryIntoDB(id, payload);
+
+  sendResponse(res, {
+    data: result,
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "succesfull updated product details category",
+  });
+});
+
 export const ProductDetailsCategoryControllers = {
   createProductDetailsCategory,
+  updateProductDetailsCategory,
 };
--- src/app/modules/productDetailsCategory/productDetailsCategory.route.ts
+++ src/app/modules/productDetailsCategory/productDetailsCategory.route.ts
@@ -5,4 +5,6 @@ const router = Router();
 
 router.post("/create", ProductDetailsCategoryControllers.createProductDetailsCategory);
 
+router.patch("/update/:id", ProductDetailsCategoryControllers.updateProductDetailsCategory);
+
 export const ProductDetailsCategoryRoutes = router;
