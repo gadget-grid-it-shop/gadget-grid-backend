2c1b3d9e6ca3405cf0d11b4171384771172dde8a
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2024-10-04 09:21:42 +0600

    check permission

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,49 +1,45 @@
-import { NextFunction, Request, Response } from "express";
+import {NextFunction, Request, Response} from "express";
 import httpStatus from "http-status";
-import { ZodError, ZodIssue } from "zod";
-import { TErrorSourse, TGenericErrorResponse } from "../interface/error.interface";
-import { handleZodError } from "../errors/handleZodError";
-import { handleCastError } from "../errors/handleCastError";
+import {ZodError, ZodIssue} from "zod";
+import {TErrorSourse, TGenericErrorResponse} from "../interface/error.interface";
+import {handleZodError} from "../errors/handleZodError";
+import {handleCastError} from "../errors/handleCastError";
 import handleDuplicateError from "../errors/handleDuplicateError";
 import handleMulterError from "../errors/handleMulterError";
 import AppError from "../errors/AppError";
 import config from "../config";
 
-
-
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   let message = err.message || "Something went wrong";
-  let statusCode = err.statusCode || 500
+  let statusCode = err.statusCode || 500;
 
-  let errorSources: TErrorSourse = [{
-    path: "",
-    message: "Something went wrong"
-  }]
+  // console.log(err);
 
+  let errorSources: TErrorSourse = [
+    {
+      path: "",
+      message: "Something went wrong",
+    },
+  ];
 
   if (err instanceof ZodError) {
-    const simplifiedError = handleZodError(err)
-    return res.status(simplifiedError.statusCode).json(simplifiedError)
-
+    const simplifiedError = handleZodError(err);
+    return res.status(simplifiedError.statusCode).json(simplifiedError);
   }
 
-
-  if (err?.name === 'CastError') {
-    const simplifiedError = handleCastError(err)
-    return res.status(simplifiedError.statusCode).json(simplifiedError)
-
+  if (err?.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    return res.status(simplifiedError.statusCode).json(simplifiedError);
   }
-
 
   if (err.code === 11000) {
-    const simplifiedError = handleDuplicateError(err)
-    return res.status(simplifiedError.statusCode).json(simplifiedError)
+    const simplifiedError = handleDuplicateError(err);
+    return res.status(simplifiedError.statusCode).json(simplifiedError);
   }
 
-
   if (err.name === "MulterError") {
-    const simplifiedError = handleMulterError(err)
-    return res.status(simplifiedError.statusCode).json(simplifiedError)
+    const simplifiedError = handleMulterError(err);
+    return res.status(simplifiedError.statusCode).json(simplifiedError);
   }
 
   if (err instanceof AppError) {
@@ -52,9 +48,8 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
       statusCode: err.statusCode,
       message: err.message,
       errorSources,
-      stack: config.node_environment === 'development' ? err?.stack : null
-    })
-
+      stack: config.node_environment === "development" ? err?.stack : null,
+    });
   }
 
   if (err instanceof Error) {
@@ -63,10 +58,9 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
       statusCode: statusCode,
       message: err.message,
       errorSources,
-      stack: config.node_environment === 'development' ? err?.stack : null
-    })
+      stack: config.node_environment === "development" ? err?.stack : null,
+    });
   }
-
 
   return res.status(statusCode).json({
     statusCode,
--- src/app/modules/Images/image.service.ts
+++ src/app/modules/Images/image.service.ts
@@ -1,126 +1,117 @@
-import { startSession } from "mongoose"
-import cloudinary from "../../lib/image/image.config"
-import { DeleteApiResponse, UploadApiErrorResponse, UploadApiResponse } from "cloudinary"
-import { TImage } from "./image.interface"
-import { Image } from "./image.model"
-import AppError from "../../errors/AppError"
-import httpStatus from "http-status"
-
+import {startSession} from "mongoose";
+import cloudinary from "../../lib/image/image.config";
+import {DeleteApiResponse, UploadApiErrorResponse, UploadApiResponse} from "cloudinary";
+import {TImage} from "./image.interface";
+import {Image} from "./image.model";
+import AppError from "../../errors/AppError";
+import httpStatus from "http-status";
 
 const uploadImageIntoDB = async (files: Express.Multer.File[], type: string, folder: string | null) => {
-    const session = await startSession()
+  const session = await startSession();
 
-    if (files.length === 0) {
-        throw new AppError(httpStatus.CONFLICT, 'Upload some files')
-    }
+  if (!files) {
+    throw new AppError(httpStatus.CONFLICT, "Failed to upload");
+  }
 
-    else {
-        files.forEach(file => {
-            if (file.size > 10485760) {
-                throw new AppError(httpStatus.CONFLICT, 'File size is too large. highest limit is 10 mb')
-            }
-        })
-    }
+  if (files?.length === 0) {
+    throw new AppError(httpStatus.CONFLICT, "Upload some files");
+  } else {
+    files.forEach((file) => {
+      if (file.size > 10485760) {
+        throw new AppError(httpStatus.CONFLICT, "File size is too large. highest limit is 10 mb");
+      }
+    });
+  }
 
-    try {
-        session.startTransaction()
+  try {
+    session.startTransaction();
 
-        const uploadImages = files.map(file => {
-            return cloudinary.uploader.upload(file.path, function (err: UploadApiErrorResponse, result: UploadApiResponse) {
-                if (err) {
-                    console.log(err)
-                    throw new AppError(httpStatus.OK, 'cloudinary upload failed')
-                }
-
-                return result
-            })
-        })
-
-        const uploadedImages: UploadApiResponse[] = await Promise.all(uploadImages).catch(err => { throw new AppError(httpStatus.CONFLICT, 'upload failed') })
-
-        if (uploadImages.length === 0) {
-            throw new AppError(httpStatus.CONFLICT, 'Failed to upload to cloudinary')
+    const uploadImages = files.map((file) => {
+      return cloudinary.uploader.upload(file.path, function (err: UploadApiErrorResponse, result: UploadApiResponse) {
+        if (err) {
+          console.log(err);
+          throw new AppError(httpStatus.OK, "cloudinary upload failed");
         }
 
-        const payloadImages: TImage[] = uploadedImages.map((image: UploadApiResponse) => (
-            {
-                extension: image.format,
-                height: image.height,
-                width: image.width,
-                image: image.url,
-                name: image.original_filename || 'backup',
-                size: image.bytes,
-                image_type: type,
-                public_id: image.public_id,
-                folder: folder
-            }
-        ))
+        return result;
+      });
+    });
 
-        const databaseResult = await Image.create(payloadImages)
+    const uploadedImages: UploadApiResponse[] = await Promise.all(uploadImages).catch((err) => {
+      throw new AppError(httpStatus.CONFLICT, "upload failed");
+    });
 
-        if (!databaseResult) {
-            throw new AppError(httpStatus.CONFLICT, 'databae upload failed')
-        }
-
-        await session.commitTransaction()
-        await session.endSession()
-        return databaseResult
+    if (uploadImages.length === 0) {
+      throw new AppError(httpStatus.CONFLICT, "Failed to upload to cloudinary");
     }
-    catch (err) {
-        console.log(err)
-        await session.abortTransaction()
-        await session.endSession()
-        throw new AppError(httpStatus.CONFLICT, err instanceof AppError ? err?.message : 'upload failed')
-    }
-}
 
+    const payloadImages: TImage[] = uploadedImages.map((image: UploadApiResponse) => ({
+      extension: image.format,
+      height: image.height,
+      width: image.width,
+      image: image.url,
+      name: image.original_filename || "backup",
+      size: image.bytes,
+      image_type: type,
+      public_id: image.public_id,
+      folder: folder,
+    }));
+
+    const databaseResult = await Image.create(payloadImages);
+
+    if (!databaseResult) {
+      throw new AppError(httpStatus.CONFLICT, "databae upload failed");
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+    return databaseResult;
+  } catch (err) {
+    console.log(err);
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(httpStatus.CONFLICT, err instanceof AppError ? err?.message : "upload failed");
+  }
+};
 
 const getAllImagesFromDB = async (parent_id: string | null) => {
+  const folder = parent_id || null;
 
-    const folder = parent_id || null
+  const result = await Image.find({folder}).sort({createdAt: 1});
+  return result;
+};
 
-    const result = await Image.find({ folder }).sort({ 'createdAt': 1 })
-    return result
-}
+const deleteImagesFromDB = async ({public_ids, database_ids}: {public_ids: string[]; database_ids: string[]}) => {
+  const session = await startSession();
 
+  try {
+    session.startTransaction();
+    const deletedFromCloud: DeleteApiResponse = await cloudinary.api.delete_resources([...public_ids], {type: "upload", resource_type: "image"});
 
-const deleteImagesFromDB = async ({ public_ids, database_ids }: { public_ids: string[], database_ids: string[] }) => {
-
-
-    const session = await startSession()
-
-    try {
-        session.startTransaction()
-        const deletedFromCloud: DeleteApiResponse = await cloudinary.api.delete_resources([...public_ids], { type: 'upload', resource_type: 'image' })
-
-        if (!deletedFromCloud) {
-            throw new AppError(httpStatus.CONFLICT, 'Failed to delete from cloud')
-        }
-
-        console.log(deletedFromCloud)
-
-        const deleteFromBD = await Image.deleteMany({ _id: { $in: [...database_ids] } })
-
-        if (!deleteFromBD) {
-            throw new AppError(httpStatus.CONFLICT, 'Failed to delete from database')
-        }
-
-        console.log(deleteFromBD)
-
-        await session.commitTransaction()
-        await session.endSession()
-
-        return deleteFromBD
+    if (!deletedFromCloud) {
+      throw new AppError(httpStatus.CONFLICT, "Failed to delete from cloud");
     }
-    catch (err) {
-        console.log(err)
-        await session.abortTransaction()
-        await session.endSession()
-        throw new AppError(httpStatus.CONFLICT, 'Delete failed')
+
+    console.log(deletedFromCloud);
+
+    const deleteFromBD = await Image.deleteMany({_id: {$in: [...database_ids]}});
+
+    if (!deleteFromBD) {
+      throw new AppError(httpStatus.CONFLICT, "Failed to delete from database");
     }
-}
 
-export const ImageUploadServices = { uploadImageIntoDB, getAllImagesFromDB, deleteImagesFromDB }
+    console.log(deleteFromBD);
 
+    await session.commitTransaction();
+    await session.endSession();
 
+    return deleteFromBD;
+  } catch (err) {
+    console.log(err);
+    await session.abortTransaction();
+    await session.endSession();
+    throw new AppError(httpStatus.CONFLICT, "Delete failed");
+  }
+};
 
+export const ImageUploadServices = {uploadImageIntoDB, getAllImagesFromDB, deleteImagesFromDB};
