12134cda8363206efad394839c6aa1e1c9f25731
MHkhan994 <khanmahmud994@gmail.com>
2026-06-22 00:52:29 +0600

    cicd test

--- src/app/middleware/notFound.ts
+++ src/app/middleware/notFound.ts
@@ -1,23 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from 'express'
-import httpStatus from 'http-status'
-import { TGenericErrorResponse } from '../interface/error.interface'
+import { NextFunction, Request, Response } from "express";
+import httpStatus from "http-status";
+import { TGenericErrorResponse } from "../interface/error.interface";
 
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-
   const errorResponse: TGenericErrorResponse = {
     success: false,
-    message: 'API not found',
-    errorSources: [{
-      path: 'api',
-      message: 'API not found',
-    }],
-    statusCode: httpStatus.NOT_FOUND
-  }
+    message: "API not found test",
+    errorSources: [
+      {
+        path: "api",
+        message: "API not found test",
+      },
+    ],
+    statusCode: httpStatus.NOT_FOUND,
+  };
 
-  return res.status(httpStatus.NOT_FOUND).json(
-    errorResponse
-  )
-}
+  return res.status(httpStatus.NOT_FOUND).json(errorResponse);
+};
 
-export default notFound
+export default notFound;
